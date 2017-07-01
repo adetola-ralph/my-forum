b0386c76a4de3d5c93e53dc3b934525be5439d91
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-01 20:50:04 +0100

    Initialise basic koa application

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const winston = require('winston');
-const db = require('./models');
+// const winston = require('winston');
+const koa = require('koa');
 
-db.sequelize
-  .authenticate()
-  .then(() => {
-    winston.info('Connection established');
-  })
-  .catch((error) => {
-    winston.error(`Error: ${error}`);
-  });
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config({ silent: true });
 
+const app = new koa();
+const port = process.env.PORT;
+
+app.use((ctx) => {
+  ctx.body = 'Hello world';
+});
+
+app.listen(port);
 const addition = () => 1 + 2;
 
 module.exports = addition;
