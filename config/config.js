f0673702c75a69aeb3ffc6179770af5097f89f0c
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-01 17:47:34 +0100

    Change sequelize config file from json to js

---
+++ config/config.js
@@ -0,0 +1,26 @@
+/* eslint no-unused-vars: off */
+const dotenv = require('dotenv').config({ silent: true });
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
