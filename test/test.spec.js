b7b02e5b3de699399a31e200586aed3ec8c1b730
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-01 15:56:57 +0100

    Setup mocha and chai for testing

---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('This is a sample test for setup', () => {
+  it('true should be true', () => {
+    expect(true).to.equal(true);
+  });
+});
