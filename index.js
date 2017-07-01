d2b9a4bb25cae52478f44ac0cb9f098aa1bbdef0
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-01 19:04:33 +0100

    Setup db connection

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const winston = require('winston');
+const db = require('./models');
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    winston.info('Connection established');
+  })
+  .catch((error) => {
+    winston.error(`Error: ${error}`);
+  });
+
 const addition = () => 1 + 2;
 
-export default addition;
+module.exports = addition;
