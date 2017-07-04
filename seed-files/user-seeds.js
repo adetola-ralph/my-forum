b0b9dc743aafb78ee94fd7176c467477b0b35bf8
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-04 07:28:25 +0100

    Add seeders for user models

--- seed-files/user-seeds.js
+++ seed-files/user-seeds.js
@@ -1,36 +1,40 @@
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
+
 module.exports = [
   {
-    name: '',
-    email: '',
-    password: '',
+    name: 'Admin',
+    email: 'admin@my-forum.me',
+    password: bcrypt.hashSync('AdminPassword', salt),
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
-    name: '',
-    email: '',
-    password: '',
+    name: 'John Doe',
+    email: 'john@doe.com',
+    password: bcrypt.hashSync('WdkwJDi', salt),
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
-    name: '',
-    email: '',
-    password: '',
+    name: 'Jane Doe',
+    email: 'Jane@doe.com',
+    password: bcrypt.hashSync('NdwJD1', salt),
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
-    name: '',
-    email: '',
-    password: '',
+    name: 'James Ray',
+    email: 'j-ray@domain.tld',
+    password: bcrypt.hashSync('TooBrightForYou', salt),
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
-    name: '',
-    email: '',
-    password: '',
+    name: 'Tiffany La',
+    email: 'La-Tiff@domain.tld',
+    password: bcrypt.hashSync('AntfSew', salt),
     createdAt: new Date(),
     updatedAt: new Date(),
   }
