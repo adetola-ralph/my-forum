c6bc92666424d40ceeeeff36c44be3839a5dda86
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-16 18:42:40 +0100

    Write tests for auth signin

--- test/authentication.spec.js
+++ test/authentication.spec.js
@@ -49,12 +49,53 @@ describe('Authentication', () => {
   });
 
   describe('User signin', () => {
-    it.skip('Should reject wrong password be able to signin', async () => {
-      await api.post('/api/v1/signin')
+    it('Should reject wrong password be able to signin', async () => {
+      const res = await api.post('/api/v1/signin')
       .send({
         email: 'admin@my-forum.me',
         password: 'WrongPassword',
       }).expect(401);
+
+      expect(res.body).to.haveOwnProperty('success');
+      expect(res.body.success).to.be.a('boolean');
+      expect(res.body.success).to.be.false;
+    });
+
+    it('Should return error for incomplete details on signup', async () => {
+      const res = await api.post('/api/v1/signin')
+      .send({
+        email: 'oreofe.olutola@gmail.com',
+      }).expect(400);
+
+      expect(res.body).to.haveOwnProperty('success');
+      expect(res.body.success).to.be.a('boolean');
+      expect(res.body.success).to.be.false;
+    });
+
+    it('Should return on error on wrong or non existent email', async () => {
+      const res = await api.post('/api/v1/signin')
+      .send({
+        email: 'admin@my-foru.me',
+        password: 'AdminPassword',
+      }).expect(401);
+
+      expect(res.body).to.haveOwnProperty('success');
+      expect(res.body.success).to.be.a('boolean');
+      expect(res.body.success).to.be.false;
+    });
+
+    it('Should allow registered users to signin', async () => {
+      const res = await api.post('/api/v1/signin')
+      .send({
+        email: 'admin@my-forum.me',
+        password: 'AdminPassword',
+      }).expect(200);
+
+      expect(res.body).to.haveOwnProperty('success');
+      expect(res.body.success).to.be.a('boolean');
+      expect(res.body.success).to.be.true;
+      expect(res.body).to.have.property('data');
+      expect(res.body.data).to.have.property('token');
     });
   });
 });
