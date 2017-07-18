e92a0fe25f9733f8102ef65c7e569cdbe0e454b3
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-07-18 21:55:16 +0100

    Add test for topic creation

--- test/topic.spec.js
+++ test/topic.spec.js
@@ -4,11 +4,12 @@ import app from './../index';
 
 const expect = chai.expect;
 const api = supertest.agent(app.listen());
+let response, token;
 
 describe('Topics', () => {
   it('all topics should be available to all users', async () => {
     const res = await api.get('/api/v1/topics')
-              .expect(200);
+                .expect(200);
 
     expect(res.body.success).to.be.true;
     expect(res.body.data).to.be.an('array');
@@ -17,6 +18,41 @@ describe('Topics', () => {
   });
 
   describe('Topic creation', () => {
-    it('should return 401 for unauthenticated creation of topics');
+    it('should return 401 for unauthenticated creation of topics', async () => {
+      await api.post('/api/v1/topics')
+      .send({
+        topicName: 'Another 10 weird Stuff',
+        userId: 2,
+      })
+      .expect(401);
+    });
+
+    before(async () => {
+      response = await api.post('/api/v1/signin')
+        .send({
+          email: 'admin@my-forum.me',
+          password: 'AdminPassword',
+        });
+      token = response.body.data.token;
+    });
+
+    it('should allow authenticated users to create topic', async () => {
+      await api.post('/api/v1/topics')
+      .set('authorization', token)
+      .send({
+        topicName: 'Another 10 weird Stuff',
+        userId: 2,
+      })
+      .expect(200);
+    });
+
+    it('should allow throw error on incomplete details', async () => {
+      await api.post('/api/v1/topics')
+      .set('authorization', token)
+      .send({
+        topicName: 'Another 10 weird Stuff',
+      })
+      .expect(400);
+    });
   });
 });
